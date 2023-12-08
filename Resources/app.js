ab9100d432808b876642643fe14986080bc7fa4c
A-Berezvay <berezvay.albert@gmail.com>
2023-12-08 15:31:22 +0100

    Icons added, Interactive header on desktop

---
+++ Resources/app.js
@@ -0,0 +1,16 @@
+// HIDDEN HEADER EFFECT WHILE SCROLLING ON DESKTOP
+
+const nav = document.querySelector ("header");
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () =>{
+    if (lastScrollY < window.scrollY) {
+        nav.classList.add("header-hidden");
+    } else {
+        nav.classList.remove("header-hidden");
+    }
+
+    lastScrollY = window.scrollY;
+});
+
+
