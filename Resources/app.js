d2f3a49be005b5e3d1816f977ca07df8de4bdcb2
A-Berezvay <berezvay.albert@gmail.com>
2024-02-08 14:15:38 +0100

    Final portfolio

--- Resources/app.js
+++ Resources/app.js
@@ -9,11 +9,21 @@ window.addEventListener("scroll", () =>{
         nav.classList.remove("header-hidden");
     }
 
-    if (window.scrollY === 0) {
-        nav.classList.add("scrolled");
-    } else {
-        nav.classList.remove("scrolled");
-    }
-
     lastScrollY = window.scrollY;
 });
+
+// SCROLLING EFFECT
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => observer.observe(el));
